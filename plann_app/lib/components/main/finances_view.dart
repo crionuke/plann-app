ac384273bf74a03ef4bda3b85008be1d6cea779b
Kirill Byvshev <k@byv.sh>
2020-10-26 22:32:41 +0300

    fix month report height

--- plann_app/lib/components/main/finances_view.dart
+++ plann_app/lib/components/main/finances_view.dart
@@ -35,7 +35,7 @@ class FinancesView extends StatelessWidget {
     return Column(children: <Widget>[
       Container(
           margin: EdgeInsets.all(10),
-          height: 425,
+          height: 415,
           child: Provider<MonthCaruselBloc>(
               create: (context) => monthCaruselBloc,
               dispose: (context, bloc) => bloc.dispose(),
